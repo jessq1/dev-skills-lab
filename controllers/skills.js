68dc6c5f027fd04d01725a7231a5b611b85a1d44
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-14 20:55:52 -0400

    add data structure

---
+++ controllers/skills.js
@@ -0,0 +1,48 @@
+import * as skillsDb from '../data/skill-db.js'
+
+
+export {
+  index,
+  newSkill as new,
+  show,
+  create,
+  deleteSkill as delete,
+}
+
+
+function index(req, res) {
+    skillsDb.find({}, function(error, skills) {
+      res.render('skills/index', {
+        skills,
+        error,
+        time: req.time
+      })
+    })
+  }
+  
+  function newSkill(req,res){
+      res.render('skills/new')
+  }
+  function deleteSkill(req, res) {
+    skillsDb.findByIdAndDelete(req.params.id, function(error, skill) {
+      res.redirect('/skills')
+    })
+  }
+  
+  function create(req,res){
+      skillsDb.create(req.body, function(error, skill) {
+      res.redirect('/skills')
+    })
+  }
+  
+  
+  function show(req, res) {
+    skillsDb.findById(req.params.id, function(error, skill) {
+      res.render('skills/show', {
+        skill,
+        error
+      })
+    })
+  }
+  
+  
--- server.js
+++ server.js
@@ -4,9 +4,10 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import methodOverride from 'method-override'
 
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as skillsRouter } from './routes/skills.js'
 
 const app = express()
 
@@ -27,8 +28,9 @@ app.use(
   )
 )
 
+app.use(methodOverride('_method'))
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/skills', skillsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
